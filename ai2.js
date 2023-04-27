ef5c67ebba8e68df917579f68d08990d329d725e
Frixnipe <96536214+Frixnipe@users.noreply.github.com>
2023-04-27 21:40:24 +0200

    Update ai2.js

--- ai2.js
+++ ai2.js
@@ -1,6 +1,6 @@
 (function(ext) {
     // Define your API credentials here
-    const openaiApiKey = process.env.OPENAI_API_KEY;
+    const openaiApiKey = '{{ secrets.OPENAI_API_KEY }}';
 
     // Define your custom block here
     ext.generateText = function(prompt, callback) {
