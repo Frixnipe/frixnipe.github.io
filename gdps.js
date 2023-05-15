8871b85e4ff5acbb54bf430a4ee52aea70056590
Frixnipe <96536214+Frixnipe@users.noreply.github.com>
2023-05-15 16:05:15 +0200

    Update gdps.js

--- gdps.js
+++ gdps.js
@@ -1,16 +1,36 @@
-const channelId = 'YOUR_CHANNEL_ID'; // Replace with the desired channel ID
+(function (Scratch) {
+  'use strict';
 
-fetch(`https://discord.com/api/v9/channels/${channelId}/messages`, {
-  method: 'GET',
-  headers: {
-    'Authorization': 'YOUR_DISCORD_BOT_TOKEN' // Replace with your Discord bot token
+  class Discord {
+    getInfo() {
+      return {
+        id: 'discord',
+        name: 'Discord',
+        blocks: [
+          {
+            opcode: 'fetchMessages',
+            blockType: Scratch.BlockType.REPORTER,
+            text: 'fetch messages from Discord channel [CHANNEL_ID]',
+            arguments: {
+              CHANNEL_ID: {
+                type: Scratch.ArgumentType.STRING,
+                defaultValue: 'CHANNEL_ID',
+              },
+            },
+          },
+        ],
+      };
+    }
+
+    fetchMessages(args) {
+      const channelId = args.CHANNEL_ID;
+      const apiUrl = `https://your-server-endpoint.com/messages/${channelId}`;
+
+      return Scratch.fetch(apiUrl)
+        .then((response) => response.text())
+        .catch(() => '');
+    }
   }
-})
-  .then(response => response.json())
-  .then(data => {
-    // Process the messages
-    const messages = data.map(message => message.content);
-    const serializedMessages = messages.join('¤');
-    console.log(serializedMessages); // Replace with your desired handling of the messages
-  })
-  .catch(error => console.error(error));
+
+  Scratch.extensions.register(new Discord());
+})(Scratch);
