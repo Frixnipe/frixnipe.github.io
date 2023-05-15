e106a5038f2327a9062998a399bef6ed0faea84d
Frixnipe <96536214+Frixnipe@users.noreply.github.com>
2023-05-15 18:33:06 +0200

    Update gdps.js

--- gdps.js
+++ gdps.js
@@ -1,36 +1,37 @@
-(function (Scratch) {
-  'use strict';
+const express = require('express');
+const fetch = require('node-fetch');
+const app = express();
+const port = 9001;
 
-  class Discord {
-    getInfo() {
-      return {
-        id: 'discord',
-        name: 'Discord',
-        blocks: [
-          {
-            opcode: 'fetchMessages',
-            blockType: Scratch.BlockType.REPORTER,
-            text: 'fetch messages from Discord channel [CHANNEL_ID]',
-            arguments: {
-              CHANNEL_ID: {
-                type: Scratch.ArgumentType.STRING,
-                defaultValue: 'CHANNEL_ID',
-              },
-            },
-          },
-        ],
-      };
+// Enable CORS headers
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+  next();
+});
+
+app.get('/messages/:channelId', (req, res) => {
+  const channelId = req.params.channelId;
+  const discordApiUrl = `https://discord.com/api/v9/channels/${channelId}/messages`;
+
+  fetch(discordApiUrl, {
+    headers: {
+      'Authorization': 'MTEwNzY1MDEwODc1NTAyNTkyMA.Go665S.ehOA_GQM6w4TBn6HyXETs2GagiJkbMgJmwNshQ' // Replace with your Discord bot token
     }
+  })
+    .then(response => response.json())
+    .then(data => {
+      const messages = data.map(message => message.content);
+      const serializedMessages = messages.join('¤');
+      res.send(serializedMessages);
+    })
+    .catch(error => {
+      console.error(error);
+      res.status(500).send('Error fetching messages from Discord.');
+    });
+});
 
-    fetchMessages(args) {
-      const channelId = args.CHANNEL_ID;
-      const apiUrl = `https://discord.com/channels/${channelId}`;
-
-      return Scratch.fetch(apiUrl)
-        .then((response) => response.text())
-        .catch(() => '');
-    }
-  }
-
-  Scratch.extensions.register(new Discord());
-})(Scratch);
+app.listen(port, () => {
+  console.log(`Discord server listening at http://localhost:${port}`);
+});
